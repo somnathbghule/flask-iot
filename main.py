5514139632a48209009bdecc46d2afb76ebd8bd1
Somnath <somnathbghule@gmail.com>
2019-01-26 07:49:30 +0530

    device name and action added

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 from flask import Flask, render_template
+ledRed="This is red led"
 app = Flask(__name__)
-#@app.route('/hello/<name>')
 @app.route('/')
-#@app.route('/server')
-#def hello(name):
-#	return name
 def index():        
     return render_template('index.html')
+@app.route('/<deviceName>/<action>')
+def do(deviceName, action):
+	if deviceName == 'ledRed':
+		actuator=ledRed
+	if action == 'on':
+		print("on the led red.")
+	if action == 'off':
+		print("off the led red.")
+	templateData={
+		'ledRed': 'On'
+	}
+	return render_template('index.html', **templateData )
+
+@app.route('/plots')
+def plots():
+	return render_template('plots.html')
+
+
 if __name__ == '__main__':       
     app.run(debug=True, host='0.0.0.0')
