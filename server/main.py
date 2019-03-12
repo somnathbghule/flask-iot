d167c5d30b6d3e8a4aa87b2e29ce6a56579f2c27
somnathbghule <somnathbghule@gmail.com>
2019-03-12 08:21:38 +0530

    adding chart samples.

--- server/main.py
+++ server/main.py
@@ -41,15 +41,12 @@ def authenticate():
    if request.method == 'POST':
        if request.form['signin'] == 'submit':
            if request.form['userId'] == 'admin' and request.form['userPassword'] == 'admin':
-               return render_template('index.html')
+               return render_template('index.html', **templateData)
            else:
-    	       return render_template('login.html')
+    	       return render_template('login.html', **templateData)
 
 @app.route('/index')
 def index():        
-    templateData={
-        'fun':myfun
-    }
     sql=SqliteDatabase(get_db, close_db)
     return render_template('index.html', **templateData)
 @app.route('/<deviceName>/<action>')
@@ -74,6 +71,34 @@ def plots():
     sql.GetAllFromEmployeeTable()
     return render_template('plots.html')
 
+@app.route('/chart')
+def chart():
+    labels = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" ];
+    values = ['Day off', 'Day off', 'Work day', 'Day off', 'Work day', 'Day off', 'Work day']
+    return render_template('chart.html', values=values, labels=labels)
+
+@app.route('/bar')
+def bar():
+    labels = ["January","February","March","April","May","June","July","August"]
+    values = [10,9,8,7,6,4,7,8]
+    return render_template('bar.html', values=values, labels=labels)
+
+@app.route('/linebar')
+def linebar():
+    labels = ["January","February","March","April","May","June","July","August"]
+    values = [10,9,8,7,6,4,7,8]
+    return render_template('linebar.html', values=values, labels=labels)
+
+@app.route('/piechart')
+def piechart():
+    #labels = ["January","February","March","April","May","June","July","August"]
+    #values = [10,9,8,7,6,4,7,8]
+    #colors = [ "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA","#ABCDEF", "#DDDDDD", "#ABCABC"  ]
+    labels = [ "Mark","John","Max","Tom" ]
+    values = [ 50, 100,150, 180 ]
+    colors = [ "#F7464A", "#46BFBD", "#FDB45C" ]
+    return render_template('piechart.html', set=zip(values, labels, colors))
+
 def get_db(db_name):
 	with app.app_context():
 		db = getattr(g, '_database', None)
@@ -93,6 +118,9 @@ def close_db():
 with app.app_context():
     LogDebug (app.name + " Application Started.")
     glob_var=100
+    templateData={
+        'fun':myfun
+    }
 
 @app.teardown_appcontext
 def teardown_app(exception):
